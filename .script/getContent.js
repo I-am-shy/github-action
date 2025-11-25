ddff4a3f9cce90c1138806bef603b31f495e1d5b
shyshi <shyshii.@zego.im>
2025-11-25 11:57:25 +0800

    feat: 更新脚本

--- .script/getContent.js
+++ .script/getContent.js
@@ -4,4 +4,19 @@ const { argv } = require('process');
 
 const filePaths = argv.slice(2);
 
-console.log(filePaths);
+console.log('本次 pr 变更的文件列表', filePaths);
+console.log('--------------------------------');
+console.log('开始读取 mdx 文件的内容:');
+for (const filePath of filePaths) {
+  if(!filePath.endsWith('.mdx')) {
+    continue;
+  }
+  try {
+    const content = fs.readFileSync(filePath, 'utf-8');
+    console.log(content);
+    console.log('--------------------------------');
+  } catch (error) {
+    console.error(error.message);
+    continue;
+  }
+}
